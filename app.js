332c83ebf5bc24ff7517d8783f4441a713cdb39e
morgan4h <lagosmorgan0@gmail.com>
2026-03-23 04:41:23 -0700

    this change the page to servie more then one website who they belong to yt channales

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('start functions')
+
+let myBoxes = document.querySelectorAll('.up-invisible');
+
+console.log(myBoxes)
+
+myBoxes[0].onclick = function() {location.href = 'https://sofiai4h-youtube.rf.gd/blog/public/pages/community.html'}
+
+
+myBoxes[1].onclick = function() {window.confirm('coming soon...')}
+
+
+myBoxes[2].onclick = function() {window.confirm('coming soon...')}
